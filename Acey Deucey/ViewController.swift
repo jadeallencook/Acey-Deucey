31995b157a8be6930564d03b84c33d771cdd5940
Jade Allen Cook <jadeallencook@gmail.com>
2017-06-23 17:30:50 -0400

    🛠 Xcode single page app template

---
+++ Acey Deucey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Acey Deucey
+//
+//  Created by Jade Cook on 6/23/17.
+//  Copyright © 2017 Jade Allen Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
