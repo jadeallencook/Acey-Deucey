9235d5c83a12947e2bda7419b239d52a0bb8b4f6
Jade Allen Cook <jadeallencook@gmail.com>
2020-11-21 12:20:15 -0600

    Completed routes and added Firebase

---
+++ src/constants/firebase-config.tsx
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyA4p2PlkF1G07valVkFGmHM6XJjCM9Z1lU',
+  authDomain: 'acey-deucey-5146b.firebaseapp.com',
+  databaseURL: 'https://acey-deucey-5146b.firebaseio.com',
+  projectId: 'acey-deucey-5146b',
+  storageBucket: 'acey-deucey-5146b.appspot.com',
+  messagingSenderId: '889693924420',
+  appId: '1:889693924420:web:2395314eb3628f18bda011',
+};
+
+export default FIREBASE_CONFIG;
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import DICE from '../constants/dice';
+import './Menu.scss';
 
 const Menu = () => {
-    return (<div>Menu</div>);
-}
+  return (
+    <div className="Menu">
+      <h1>
+        {DICE[1]}
+        {DICE[2]}
+      </h1>
+      <button>Create Game</button>
+      <p>OR</p>
+      <form>
+        <input type="number" placeholder="Enter ID" />
+        <input type="submit" value="Join Game" />
+      </form>
+      <footer>Developed by @jadeallencook</footer>
+    </div>
+  );
+};
 
 export default Menu;
